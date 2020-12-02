97afb8b78cec41fa8d13cb22a7c132fcabc99cb8
ixianren <649291929@qq.com>
2020-12-03 00:05:58 +0800

    first commit

---
+++ New Unity Project/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float limit;
+    public float speed;
+    int intFlag = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        //当Z轴位置大于limit的时候就向负方向移动
+        if (transform.position.z >= limit)
+        {
+            intFlag = -1;
+        }
+        //当Z轴位置小于-limit的时候就向正方向移动
+        if (transform.position.z < -limit)
+        {
+            intFlag = 1;
+        }
+
+        transform.Translate(new Vector3(0, 0, intFlag) * Time.deltaTime * speed);
+    }
+}
---
+++ New Unity Project/Assets/drawline.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawline : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] spheres;
+    private LineRenderer Line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Line = GetComponent<LineRenderer>();
+        Line.material = new Material(Shader.Find("Sprites/Default"));
+        Line.startWidth = 0.3f;
+        Line.endWidth = 0.3f;
+        //Line.material.color = Color.green;
+        Line.startColor = Color.green;
+        Line.endColor = Color.green;
+        Line.positionCount = spheres.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Line.SetPosition(0, spheres[0].position);
+        Line.SetPosition(1, spheres[1].position);
+        Line.SetPosition(2, spheres[2].position);
+        Line.SetPosition(3, spheres[3].position);
+    }
+}
